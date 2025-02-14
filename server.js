350c119dc5a3a815e171004479039fe10c9d0938
avinashdahariya <avinash@radlabs.tech>
2025-02-14 16:31:07 +0530

    update

--- server.js
+++ server.js
@@ -28,9 +28,10 @@ app.get('/', (req, res) => {
 
 app.use("/api", emailRoutes);
 
+export default app;
 
 // Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
