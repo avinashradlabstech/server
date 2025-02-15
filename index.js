968fe6fbdde8bca2ec7f1921c4367a833b77e837
avinashdahariya <avinash@radlabs.tech>
2025-02-15 11:00:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
 import express from 'express';
-import { VercelRequest, VercelResponse } from '@vercel/node'
-//import emailRoutes from './app/routes/emailRoutes.js'
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-import cors from "cors";
-// Initialize Express app
 const app = express();
 
-
-// app.use(cors({
-//     origin: ['https://payapi-multipage-website.vercel.app', 'http://localhost:5173'], 
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true,
-//   }));
-
-
-// Middleware to parse JSON data in the request body
-app.use(express.json()); // For JSON payloads
-
-// Middleware to parse URL-encoded data (optional)
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({
-      message: 'Welcome to our API!',
-    });
-  });
+  res.json({ message: 'Welcome to our API!' });
+});
 
-//app.use("/api", emailRoutes);
-
-
-// Export the Express app as a serverless function
 export default (req, res) => app(req, res);
-
-
-// Start the server
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
